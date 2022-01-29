31d44cbd496078972754b2f5e06b25d28ddcb912
Oleksandr Bogdanov <oleksandr.bogdanov@grandcentrix.net>
2022-01-29 19:38:59 +0100

    inline a parents Dictionary into a func:calculateShotestPath

--- GrokAlgorithms.playground/Pages/Dijkstra.xcplaygroundpage/Contents.swift
+++ GrokAlgorithms.playground/Pages/Dijkstra.xcplaygroundpage/Contents.swift
@@ -16,10 +16,10 @@ costs["Start"] = 0
 costs["Finish"] = Double.infinity
 
 
-var parents = [String: String]()
-var processed: [String] = [String]()
-
 func calculateShotestPath() {
+    var parents = [String: String]()
+    var processed: [String] = [String]()
+    
     var node = findLowestCostNode(in: costs, exclude: processed)
     while node != nil {
         let cost = costs[node!]
@@ -34,31 +34,24 @@ func calculateShotestPath() {
         processed.append(node!)
         node = findLowestCostNode(in: costs, exclude: processed)
     }
+    
+    print(costs)
+    print(parents)
 }
 
-func findLowestCostNode(in costs: [String: Double], exclude: [String]) -> String? {
+func findLowestCostNode(in costs: [String: Double], exclude processed: [String]) -> String? {
     var lowestCost = Double.infinity
     var lowestCostNode: String? = nil
-    // Go through each node.
+
     for node in costs {
         let cost = node.value
-        // If it's the lowest cost so far and hasn't been processed yet...
         if (cost < lowestCost) && !processed.contains(node.key) {
-            // ... set it as the new lowest-cost node.
             lowestCost = cost
             lowestCostNode = node.key
         }
-        
     }
     return lowestCostNode
 }
 
 calculateShotestPath()
-print("Cost from the start to each node:")
-print(costs)
-print(parents)
-
-
-extension String: Error {}
-
 
