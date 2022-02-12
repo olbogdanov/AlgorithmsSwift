f7da17242c9751b7bd1a62320b957820b81e4c02
Oleksandr Bogdanov <oleksandr.bogdanov@grandcentrix.net>
2022-02-12 23:32:16 +0100

    optimized merge sort - reduced array allocation

--- GrokAlgorithms.playground/Pages/MergeSort.xcplaygroundpage/Contents.swift
+++ GrokAlgorithms.playground/Pages/MergeSort.xcplaygroundpage/Contents.swift
@@ -38,8 +38,55 @@ func merge(_ leftArray: [Int], _ rightArray: [Int]) -> [Int] {
     }
     return sortedArray
 }
-
 let unsortedArray = [1, 10, 30, 22, -1, 100, 40, 50, 2, 10000, 200000, -200, -1000000]
 print(mergeSort(unsortedArray))
 
+
+/// optimized merge sort - reduced array allocation
+
+var aux: [Int] = [] // auxiliary array for merges
+
+func runOptimizedMergeSort(_ array: inout [Int]) {
+    aux = Array(repeating: 0, count: array.count) // Allocate space just once
+    sort(&array, 0, array.capacity - 1)
+}
+
+func sort(_ array: inout [Int], _ lo: Int, _ hi: Int) {
+    if hi <= lo {
+        return
+    }
+    let mid = lo + (hi - lo) / 2
+    sort(&array, 0, mid)
+    sort(&array, mid + 1, hi)
+    merge(&array, lo, mid, hi)
+}
+
+func merge(_ array: inout [Int], _ lo: Int, _ mid: Int, _ hi: Int) {
+    var i = lo, j = mid + 1
+    
+    for k in lo...hi {
+        aux[k] = array[k]
+    }
+    
+    for k in lo...hi {
+        if i > mid {
+            array[k] = aux[j]
+            j += 1
+        }
+        else if j > hi {
+            array[k] = aux[i]
+            i += 1
+        } else if aux[j] < aux[i] {
+            array[k] = aux[j]
+            j += 1
+        } else {
+            array[k] = aux[i]
+            i += 1
+        }
+    }
+}
+
+var uArray = [1, 10, 30, 22, -1, 100, 40, 50, 2, 10000, 200000, -200, -1000000]
+runOptimizedMergeSort(&uArray)
+print(uArray) /// result
 
