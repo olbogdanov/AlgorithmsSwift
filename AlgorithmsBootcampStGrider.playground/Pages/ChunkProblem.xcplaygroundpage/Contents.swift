5799fc5f19f2241a8800db997716a331f52c5def
Oleksandr Bogdanov <oleksandr.bogdanov@grandcentrix.net>
2022-02-22 01:30:37 +0100

    Chunk problem 2 solution

--- AlgorithmsBootcampStGrider.playground/Pages/ChunkProblem.xcplaygroundpage/Contents.swift
+++ AlgorithmsBootcampStGrider.playground/Pages/ChunkProblem.xcplaygroundpage/Contents.swift
@@ -11,5 +11,17 @@ func chunk(_ array: [Int], _ size: Int) -> [[Int]] {
     return arrayOfChanks
 }
 
+func chunk2(_ array: [Int], _ size: Int) -> [[Int]] {
+    var arrayOfChanks = [[Int]]()
+    for value in array {
+        let last = arrayOfChanks.last
+        if last == nil || last?.count == size {
+            arrayOfChanks.append([Int]())
+        }
+        arrayOfChanks[arrayOfChanks.count-1].append(value)
+    }
+    return arrayOfChanks
+}
+
 let chunk = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-print(chunk(chunk, 4))
+print(chunk(chunk, 6))
