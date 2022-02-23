d3a23c6733e34994b16334d3e1dbb7c6efe517e7
Oleksandr Bogdanov <oleksandr.bogdanov@grandcentrix.net>
2022-02-23 01:07:39 +0100

    Chunk problem solution 3

--- AlgorithmsBootcampStGrider.playground/Pages/ChunkProblem.xcplaygroundpage/Contents.swift
+++ AlgorithmsBootcampStGrider.playground/Pages/ChunkProblem.xcplaygroundpage/Contents.swift
@@ -3,7 +3,7 @@ import Foundation
 func chunk(_ array: [Int], _ size: Int) -> [[Int]] {
     var arrayOfChanks = [[Int]]()
     for (index, value) in array.enumerated() {
-        if (index % size == 0) {
+        if index % size == 0 {
             arrayOfChanks.append([Int]())
         }
         arrayOfChanks[arrayOfChanks.count-1].append(value)
@@ -23,5 +23,16 @@ func chunk2(_ array: [Int], _ size: Int) -> [[Int]] {
     return arrayOfChanks
 }
 
+func chunk3(_ array: [Int], _ size: Int) -> [[Int]] {
+    var arrayOfChanks = [[Int]]()
+    var i = 0
+    while i < array.count {
+        let endIndex = min(i+size, array.count)
+        arrayOfChanks.append(Array(array[i..<endIndex]))
+        i += size
+    }
+    return arrayOfChanks
+}
+
 let chunk = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-print(chunk(chunk, 6))
+print(chunk3(chunk, 7))
