6d937855d3e3d24f50a668b056fe05bfae831f89
Oleksandr Bogdanov <oleksandr.bogdanov@grandcentrix.net>
2022-04-04 23:33:25 +0200

    anagrams ingored case + ignored non alpahanumeric chars

---
+++ AlgorithmsBootcampStGrider.playground/Pages/Anagrams.xcplaygroundpage/Contents.swift
@@ -0,0 +1,59 @@
+import Foundation
+
+//anagrams ingored case, ignored non alpahanumeric chars
+func anagrams(_ stringA: String, _ stringB: String) -> Bool {
+    let strA = stringA.replacingOccurrences(of: "[^\\w]", with: "", options: [.regularExpression])
+    let strB = stringB.replacingOccurrences(of: "[^A-Za-z0-9]", with: "", options: [.regularExpression]).lowercased()
+
+    if strA.count != strB.count {
+        return false
+    }
+    var mapA: [Character: Int] = [:]
+    strA.forEach { char in
+        let count = mapA[char]
+        mapA[char] = count == nil ? 1 : count! + 1
+    }
+    var mapB: [Character: Int] = [:]
+    strB.forEach { char in
+        let count = mapB[char]
+        mapB[char] = count == nil ? 1 : count! + 1
+    }
+
+    for (key, value) in mapA {
+        if mapB[key] != value {
+            return false
+        }
+    }
+    return true
+}
+
+print(anagrams("na   ba.. !", "ba -a ,  n"))
+assert(anagrams("aba   ", "baa!") == true)
+assert(anagrams("ab", "baa") == false)
+assert(anagrams("baa", "ab") == false)
+
+//kotlin
+//val re = Regex("[^A-Za-z0-9 ]")
+//answer = re.replace(str, "")
+//check if char:
+//Char.isLetterOrDigit()
+//get char from string:
+//var b = "a"
+//var c = b[0]
+
+//swift
+//str.replacingOccurrences(of: "[^A-Za-z0-9 ]", with: "", options: [.regularExpression])
+//or
+//str.components(separatedBy: CharacterSet.symbols).joined(separator: "")
+//check if num:
+//character.isWholeNumber
+
+//java
+//str.replaceAll("[^a-zA-Z0-9]", " ");
+//check if char:
+//Character.isLetterOrDigit(string.charAt(index))
+//get char from string
+//s.charAt(0)
+
+//pattern includes all chars of all languages
+//^[{\p{L}}{0-9}]$
